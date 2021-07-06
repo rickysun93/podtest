8ddd8e4901c47d0e37eb85a44e00a5bbc8555fe3
Haoran Sun <haorsun@microsoft.com>
2021-07-06 12:51:08 +0800

    init

---
+++ podtest/podtest/Program.cs
@@ -0,0 +1,28 @@
+﻿using k8s;
+using System;
+
+namespace podtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = KubernetesClientConfiguration.InClusterConfig();
+            IKubernetes client = new Kubernetes(config);
+            Console.WriteLine("Starting Request!");
+
+            var list = client.ListNamespacedPod("default");
+            foreach (var item in list.Items)
+            {
+                Console.WriteLine(item.Metadata.Name);
+            }
+
+            if (list.Items.Count == 0)
+            {
+                Console.WriteLine("Empty!");
+            }
+        }
+    }
+}
